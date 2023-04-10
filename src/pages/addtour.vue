2fd022a64af8a71ab02d361afc106176c0892fe2
jane99j <jaynerrch@gmail.com>
2023-04-10 16:38:47 +0700

    edite label

--- src/pages/addtour.vue
+++ src/pages/addtour.vue
@@ -98,7 +98,7 @@
                         </div>
                         <div>
                             <label for="category"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ภาษี (0% 7% 9%)</label>
                             <select id="category"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option select="">0%</option>
