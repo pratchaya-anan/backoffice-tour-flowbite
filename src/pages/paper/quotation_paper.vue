2bfb4d4fe6b6e99553233d00710996c733839209
OB02CGTH <sornsiri1208@gmail.com>
2023-04-18 09:24:56 +0700

    Set size paper

---
+++ src/pages/paper/quotation_paper.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="bg-gray-100">
+    <div style="width: 210mm; min-height: 297mm; padding: 2mm; margin: 0 auto; border: 1px #d3d3d3 solid; border-radius: 5px; background: white; box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);">
+      <!-- <div class=""></div> -->
+    </div>
+  </section>
+  <!-- <div class="flex bg-gray-900"></div> -->
+</template>
