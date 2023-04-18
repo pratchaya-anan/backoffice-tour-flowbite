cd63c2f27434f1427d7023c770c633c2ef794eda
OB02CGTH <sornsiri1208@gmail.com>
2023-04-18 14:27:13 +0700

    Succes head quotation paper

--- src/pages/paper/quotation_paper.vue
+++ src/pages/paper/quotation_paper.vue
@@ -1,9 +1,36 @@
 <template>
   <section class="bg-gray-100">
-    <div style="width: 210mm; min-height: 297mm; padding: 2mm; margin: 0 auto; border: 1px #d3d3d3 solid; border-radius: 5px; background: white; box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);">
-      <div class="grid grid-rows-3 m-px-2">
-        <div class="cols-span-1">
-          <img class="w-25 h-auto" src="../../assets/images/ll-01.png" alt="Logo">
+    <div
+      style="width: 210mm; min-height: 297mm; padding: 2mm; margin: 0 auto; border: 1px #d3d3d3 solid; border-radius: 5px; background: white; box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);">
+      <div class="grid grid-rows-4 m-5">
+        <img class="w-25" src="../../assets/images/ll-01.png" alt="Logo">
+        <div class="grid grid-cols-3">
+          <div class="col-span-2">
+            <div>ห้างหุ้นส่วนจำกัด นิลผกา</div>
+            <div>
+              49/175 ม.5 ต.หนองบัว อ.เมืองอุดรธานี จ.อุดรธานี 41000
+            </div>
+            <div>
+              เลขประจำตัวผู้เสียภาษี: 413558001156 | โทร.098-1047644
+            </div>
+            <div class="text-blue-400">
+              www.facebook.com/Jampahomtour
+            </div>
+          </div>
+          <div class="grid grid-cols-3">
+            <div class="col-span-3 font-bold text-center">ใบเสนอราคา/ยืนยันการสั่งซื้อ</div>
+            <div class="col-span-3 text-center">Quatation</div>
+            <div class="font-bold">เลขที่:</div>
+            <div class="col-span-2 text-right">0841142174178</div>
+            <div class="font-bold">วันที่:</div>
+            <div class="col-span-2 text-right">24/03/2566</div>
+          </div>
+          <hr class="col-span-3">
+        </div>
+        <div class="grid grid-cols-3">
+          <div class="col-span-2">
+            <div class="font-bold">วันที่:</div>
+          </div>
         </div>
       </div>
     </div>
