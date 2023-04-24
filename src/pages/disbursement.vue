ecfe9cbd73f6bfff18f7f5a5d48d8545a36414e8
jane99j <jaynerrch@gmail.com>
2023-04-24 17:53:55 +0700

    edit disbursement

--- src/pages/disbursement.vue
+++ src/pages/disbursement.vue
@@ -205,7 +205,7 @@
                 </div>
                 <!-- Main modal -->
                 <div id="updateProductModal"
-                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                    class="mt-10 hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                     <div class="relative p-4 w-full max-w-screen-xl h-full md:h-auto">
                         <!-- Modal content -->
                         <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
