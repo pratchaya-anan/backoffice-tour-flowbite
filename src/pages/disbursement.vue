a18a7d9a4c460a46daadae0c7c3b0897d3461e39
jane99j <jaynerrch@gmail.com>
2023-04-24 18:00:33 +0700

    edit disbursement

--- src/pages/disbursement.vue
+++ src/pages/disbursement.vue
@@ -205,7 +205,7 @@
                 </div>
                 <!-- Main modal -->
                 <div id="updateProductModal"
-                    class="mt-10 hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                    class=" hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                     <div class="relative p-4 w-full max-w-screen-xl h-full md:h-auto">
                         <!-- Modal content -->
                         <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
@@ -410,7 +410,7 @@
                                                             clip-rule="evenodd"></path>
                                                     </svg>
                                                 </td>
-                                            </tr>
+                                            </tr>                                            
                                             <tr class="bg-gray-50 border-b dark:bg-gray-900 dark:border-gray-700">
                                                 <th scope="row"
                                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -428,31 +428,7 @@
                                                 <td class="px-6 py-4">
 
                                                 </td>
-                                            </tr>
-                                            <tr class="bg-white-50 border-b dark:bg-gray-900 dark:border-gray-700">
-                                                <th scope="row"
-                                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                    16-05-66
-                                                </th>
-                                                <td class="px-6 py-4">
-                                                    ค่ารถท้องถิ่น
-                                                </td>
-                                                <td class="px-6 py-4">-
-                                                </td>
-                                                <td class="px-6 py-4">-
-                                                </td>
-                                                <td class="px-6 py-4">6000 .-
-                                                </td>
-                                                <td class="px-6 py-4">
-                                                    <svg class="mr-1 -ml-1 w-5 h-5 text-red-500 hover:text-red-800"
-                                                        fill="currentColor" viewBox="0 0 20 20"
-                                                        xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd"
-                                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                            clip-rule="evenodd"></path>
-                                                    </svg>
-                                                </td>
-                                            </tr>
+                                            </tr>                                            
                                         </tbody>
                                     </table>
                                 </div>
