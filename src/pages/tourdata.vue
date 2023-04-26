3283da00deb5fdf161dd07c5c6c3969dabf554d2
pratchaya-anan <pratchaya-anan.sr.62@ubu.ac.th>
2023-04-26 09:33:13 +0700

    update page tourdata

--- src/pages/tourdata.vue
+++ src/pages/tourdata.vue
@@ -711,13 +711,13 @@
               </NuxtLink>
             </button>
 
-            <button
-              href="/sticker"
+            <NuxtLink
+              href="/paper/sticker"
               type="button"
               class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm ml-2 px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
             >
               พิมพ์รายชื่อติด Tag
-            </button>
+            </NuxtLink>
 
             <!-- Main modal -->
             <div
