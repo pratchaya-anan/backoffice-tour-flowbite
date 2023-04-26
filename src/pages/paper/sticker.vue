39453440c73348903819ee5d75cd025fd73a2f8a
OB02CGTH <sornsiri1208@gmail.com>
2023-04-26 12:12:38 +0700

    Setup install pinia

--- src/pages/paper/sticker.vue
+++ src/pages/paper/sticker.vue
@@ -3,7 +3,7 @@
     <div class="w-[210mm] h-min-[297mm] p-[5mm] my-1 mx-auto border-1 solid rounded bg-white drop-shadow">
 
       <div class="grid grid-cols-2">
-        <div v-for="i in 200" class="border text-center">
+        <div v-for="i in 2" class="border text-center">
           <div class="text-lg font-semibold pt-2">นันทิกาณต์ ศรีชัยมูล</div>
           <div class="text-sm text-blue-800">Nunthikan Srechaimoon</div>
           <div class="bg-yellow-200 text-sm h-7 flex justify-center content-center">กาญจนบุรี-เพชรบุรี</div>
@@ -19,8 +19,7 @@
         </div>
       </div>
 
-    </div>
-    
+    </div> 
   </section>
   <!-- <div class="flex bg-gray-900"></div> -->
 </template>
