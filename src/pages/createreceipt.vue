7007d59cb3ea5a77375efc2acc51ffe310f3eb27
pratchaya-anan <pratchaya-anan.sr.62@ubu.ac.th>
2023-05-01 18:13:25 +0700

    update page createreceipt

---
+++ src/pages/createreceipt.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div>
+      <div>
+        <div
+          href="#"
+          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            Noteworthy technology acquisitions 2021
+          </h5>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
