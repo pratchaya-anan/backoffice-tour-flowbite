807e71f1d4332a01737b8e313f8450d959e800de
pratchaya-anan <pratchaya-anan.sr.62@ubu.ac.th>
2023-05-02 14:33:43 +0700

    update header

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav
+      class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 border-b shadow-md"
+    >
       <div
         class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
       >
--- src/pages/createreceipt.vue
+++ src/pages/createreceipt.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-gray-100 h-min-228 p-3">
+  <section class="p-5">
     <div class="mx-auto max-w-screen-xl">
       <div>
         <div
@@ -65,78 +65,91 @@
                     </div>
                     <!-- Modal body -->
                     <div class="p-6 space-y-6">
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
+                      <div
+                        class="relative overflow-x-auto shadow-md sm:rounded-lg"
                       >
-                        With less than a month to go before the European Union
-                        enacts new consumer privacy laws for its citizens,
-                        companies around the world are updating their terms of
-                        service agreements to comply.
-                      </p>
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
-                      >
-                        The European Union’s General Data Protection Regulation
-                        (G.D.P.R.) goes into effect on May 25 and is meant to
-                        ensure a common set of data rights in the European
-                        Union. It requires organizations to notify users as soon
-                        as possible of high-risk data breaches that could
-                        personally affect them.
-                      </p>
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
-                      >
-                        With less than a month to go before the European Union
-                        enacts new consumer privacy laws for its citizens,
-                        companies around the world are updating their terms of
-                        service agreements to comply.
-                      </p>
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
-                      >
-                        The European Union’s General Data Protection Regulation
-                        (G.D.P.R.) goes into effect on May 25 and is meant to
-                        ensure a common set of data rights in the European
-                        Union. It requires organizations to notify users as soon
-                        as possible of high-risk data breaches that could
-                        personally affect them.
-                      </p>
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
-                      >
-                        With less than a month to go before the European Union
-                        enacts new consumer privacy laws for its citizens,
-                        companies around the world are updating their terms of
-                        service agreements to comply.
-                      </p>
-                      <p
-                        class="text-base leading-relaxed text-gray-500 dark:text-gray-400"
-                      >
-                        The European Union’s General Data Protection Regulation
-                        (G.D.P.R.) goes into effect on May 25 and is meant to
-                        ensure a common set of data rights in the European
-                        Union. It requires organizations to notify users as soon
-                        as possible of high-risk data breaches that could
-                        personally affect them.
-                      </p>
+                        <table
+                          class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
+                        >
+                          <thead
+                            class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                          >
+                            <tr>
+                              <th scope="col" class="p-4">
+                                <div class="flex items-center">
+                                  <input
+                                    id="checkbox-all-search"
+                                    type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                  />
+                                  <label
+                                    for="checkbox-all-search"
+                                    class="sr-only"
+                                    >checkbox</label
+                                  >
+                                </div>
+                              </th>
+                              <th scope="col" class="px-6 py-3">วันที่</th>
+                              <th scope="col" class="px-6 py-3">
+                                เลขที่ใบกำกับภาษี
+                              </th>
+                              <th scope="col" class="px-6 py-3">รายละเอียด</th>
+                              <th scope="col" class="px-6 py-3">จำนวนเงิน</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr
+                              v-for="list in 5"
+                              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                            >
+                              <td class="w-4 p-4">
+                                <div class="flex items-center">
+                                  <input
+                                    id="checkbox-table-search-1"
+                                    type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                  />
+                                  <label
+                                    for="checkbox-table-search-1"
+                                    class="sr-only"
+                                    >checkbox</label
+                                  >
+                                </div>
+                              </td>
+                              <th
+                                scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                              >
+                                30/03/2566
+                              </th>
+                              <td class="px-6 py-4">4226888</td>
+                              <td class="px-6 py-4">
+                                ลูกค้า-ศศิศ วิรัตน์จินดา ที่อยู่-111/0
+                                ถนนบ้านเตย อำเภอศรีวิชัย จังหวัดสุรินทร์ 25100
+                              </td>
+                              <td class="px-6 py-4">59920</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
                     </div>
                     <!-- Modal footer -->
                     <div
-                      class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
+                      class="flex justify-end items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
                     >
-                      <button
-                        data-modal-hide="extralarge-modal"
-                        type="button"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                      >
-                        I accept
-                      </button>
                       <button
                         data-modal-hide="extralarge-modal"
                         type="button"
                         class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                       >
-                        Decline
+                        ยกเลิก
+                      </button>
+                      <button
+                        data-modal-hide="extralarge-modal"
+                        type="button"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        ตกลง
                       </button>
                     </div>
                   </div>
