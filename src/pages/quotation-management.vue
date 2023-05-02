b5d4509f81147e0b1c3f5b1f4c8b5b90fbe17550
jane99j <jaynerrch@gmail.com>
2023-05-02 14:38:12 +0700

    edit

--- src/pages/quotation-management.vue
+++ src/pages/quotation-management.vue
@@ -131,8 +131,7 @@
                           >ใบเสนอราคา</a
                         >
 
-                        <a
-                          @click="setid(datadis.tourid)"
+                        <a                          
                           class="ml-3 font-medium text-blue-600 dark:text-blue-500 hover:underline"
                         >
                           ใบแจ้งหนี้/ใบกำกับภาษี</a
