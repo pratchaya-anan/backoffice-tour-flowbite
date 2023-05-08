694e1780d73fae9ff2f00cff99226c44dbcaedc9
Rocky0191 <107449941+Rocky0191@users.noreply.github.com>
2023-05-08 17:09:49 +0700

    update Page tourdata

--- src/pages/tourdata.vue
+++ src/pages/tourdata.vue
@@ -760,27 +760,51 @@
                             <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
                               ตรวจลงตราเลขที: 455/55
                             </p>
+
+                          
                             <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                              หมายเหตุ: asddds
+                              หมายเหตุ: ห้องน้ำไม่สะอาด
                             </p>
                           </div>
+                          
                           <!-- Modal footer -->
                           <div
                             class="flex items-center justify-between p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <div class="flex justify-between">
-                              <div>
-                                <input type="text" id="small-input"
-                                  class="mr-2 w-70 block w-full py-2.5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            <div>
+                              <div class="w-50">
+                          <label for="startenddate"
+                            class="block mb-2 text-sm font-medium text-gray-500 dark:text-gray-400">วันที่คอมเมนต์</label>
+                          <div date-rangepicker class="flex items-center">
+                            <div class="w-full relative">
+                              <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                  <path fill-rule="evenodd"
+                                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                    clip-rule="evenodd"></path>
+                                </svg>
                               </div>
+                              <input name="start" type="text"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="เลือกวันที่" />
+                            </div>
+                          </div>
+                        </div>
+                              </div>
+                              <div class="pt-7">
+                                <input type="text" id="small-input"
+                                  class="mr-2 w-50 block w-full py-3 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                              </div>
+                              <div class="pt-7">
                               <button data-modal-hide="staticModal" type="button"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                class="mr-6  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 คอมเมนต์
                               </button>
-                            </div>
                             <button data-modal-hide="staticModal" type="button"
                               class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                               ปิด
                             </button>
+                            </div>
                           </div>
                         </div>
                       </div>
