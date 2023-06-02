4d4ff0d6793cf2b01aacd9191b155c1ecd743403
pratchaya-anan <pratchaya-anan.sr.62@ubu.ac.th>
2023-06-02 16:40:51 +0700

    dfsf

--- src/pages/addhotel.vue
+++ src/pages/addhotel.vue
@@ -1,6 +1,107 @@
 <template>
   <section>
     <div>
+      <div
+        class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow"
+      >
+        <ol
+          class="flex justify-center items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0"
+        >
+          <li
+            class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5"
+          >
+            <span
+              class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400"
+            >
+              1
+            </span>
+            <span>
+              <h3 class="font-medium leading-tight">ข้อมูลทัวร์</h3>
+            </span>
+            <svg
+              aria-hidden="true"
+              class="w-4 h-4 ml-2 sm:ml-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 5l7 7-7 7M5 5l7 7-7 7"
+              ></path>
+            </svg>
+          </li>
+          <li
+            class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5"
+          >
+            <span
+              class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400"
+            >
+              2
+            </span>
+            <span>
+              <h3 class="font-medium leading-tight">ข้อมูลไกด์</h3>
+            </span>
+            <svg
+              aria-hidden="true"
+              class="w-4 h-4 ml-2 sm:ml-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 5l7 7-7 7M5 5l7 7-7 7"
+              ></path>
+            </svg>
+          </li>
+          <li
+            class="flex items-center text-blue-600 dark:text-blue-500 space-x-2.5"
+          >
+            <span
+              class="flex items-center justify-center w-8 h-8 text-white bg-blue-600 rounded-full shrink-0 dark:border-blue-500"
+            >
+              3
+            </span>
+            <span>
+              <h3 class="font-medium leading-tight">ข้อมูลโรงแรม</h3>
+            </span>
+            <svg
+              aria-hidden="true"
+              class="w-4 h-4 ml-2 sm:ml-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 5l7 7-7 7M5 5l7 7-7 7"
+              ></path>
+            </svg>
+          </li>
+          <li
+            class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5"
+          >
+            <span
+              class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400"
+            >
+              4
+            </span>
+            <span>
+              <h3 class="font-medium leading-tight">ข้อมูลพาหนะ</h3>
+            </span>
+          </li>
+        </ol>
+      </div>
       <div class="flex justify-center p-5">
         <div
           class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
