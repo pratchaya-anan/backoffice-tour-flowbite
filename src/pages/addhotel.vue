dd45b97e7409d403a5b81f02f50fcd7d8587d60b
pratchaya-anan <pratchaya-anan.sr.62@ubu.ac.th>
2023-06-02 16:30:19 +0700

    update

---
+++ src/pages/addhotel.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div>
+      <div class="flex justify-center p-5">
+        <div
+          class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+        >
+          <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+            Go to this step by step guideline process on how to certify for your
+            weekly benefits:
+          </p>
+          <a
+            href="#"
+            class="inline-flex items-center text-blue-600 hover:underline"
+          >
+            See our guideline
+            <svg
+              class="w-5 h-5 ml-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+              ></path>
+              <path
+                d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
